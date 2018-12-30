d680cd1e352960be7732469c37de690535e846c1
phuonglinhnguyen <nplinh.iuh@gmail.com>
2018-12-30 16:55:48 +0700

    [Test] edit some code

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <header className="app__header">dfsf</header>
-        <main></main>
+        <header className="app__header">cao minh hung</header>
+        <main>nguyen phuong linh</main>
         <footer></footer>
       </div>
     );
