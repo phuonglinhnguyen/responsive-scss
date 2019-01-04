0e19caae091b794b06fb668543731537f4084bfb
phuonglinhnguyen <nplinh.iuh@gmail.com>
2019-01-04 12:22:36 +0700

    responsive 1 nửa trang

--- src/App.js
+++ src/App.js
@@ -8,117 +8,129 @@ class App extends Component {
     return (
       <div className="app">
         <header className="app__header">
-          <div className="app__header__logo">
-            <img className="app__header__logo__img" src={logo} />
+          <div className="logo">
+            <img className="logo__img" src={logo} />
           </div>
-          <div className="app__header__nav">
-            <div className="app__header__nav__menu">
-              <div className="app__header__nav__menu__bar1"></div>
-              <div className="app__header__nav__menu__bar2"></div>
-              <div className="app__header__nav__menu__bar3"></div>
+          <div className="nav">
+            <div className="nav__btn">
+              <div className="nav__btn__bar1"></div>
+              <div className="nav__btn__bar2"></div>
+              <div className="nav__btn__bar3"></div>
+            </div>
+            <div className="nav__menu">
+              <a href="#">Home</a>
+              <a href="#">Explore</a>
+              <a href="#">Pricing</a>
+              <a href="#">Services</a>
+              <a href="#">Team</a>
+              <a href="#">Sign up free</a>
             </div>
           </div>
         </header>
         <main className="app__main">
-          <div className="app__main__intro">
-            <div className="app__main__intro__content">
-              <h1 className="font-1">RWD - RESPONSIVE WEB DESIGN</h1>
-              <p className="font-2">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum socilis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+          <div className="rwd">
+            <div className="rwd__content">
+              <h1 className="rwd__title">RWD - RESPONSIVE WEB DESIGN</h1>
+              <p className="rwd__text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum socilis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
             </div>
-            <div className="app__main__intro__download">
-              <div className="btn"><button className="btn__demo" type="submit">Demo</button></div>
-              <div className="btn"><button className="btn__download" type="submit">Download</button></div>
+            <div className="rwd__buttons">
+              <button className=" btn rwd__buttons__demo" type="submit">Demo</button>
+              <button className="btn rwd__buttons__download" type="submit">Download</button>
             </div>
           </div>
-          <div className="app__main__explore">
-            <div className="app__main__explore__content">
-              <h1 className="font-5">Explore Our Products</h1>
-              <p className="font-4">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum socilis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. </p>
-              < hr className="gachphancach" />
+          <div className="explore">
+            <div className="explore__content">
+              <h1 className="explore__title">Explore Our Products</h1>
+              <p className="explore__text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum socilis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. </p>
+              <hr className="gachphancach" />
             </div>
-            <div className="app__main__explore__work1">
-              <div className="app__main__explore__work1__content">
-                <div className="img_work1">
-                  <p className="font-3">WORK1</p>
+            <div className="explore__work1">
+              <div className="work1">
+                <div className="work1__img">
+                  <p className="work1__text">WORK1</p>
                 </div>
-                <div className="item_work1">
-                  <div className="item_work1__title">
-                    <span><p><i class="fa fa-users"></i> Real Project For Real Solutions</p></span>
+                <div className="work1__content">
+                  <div className="grid-1">
+                    <div className="work1__item">
+                      <div className="work1__item__title">
+                        <span><p><i class="fa fa-users"></i> Real Project For Real Solutions</p></span>
+                      </div>
+                      <div className="work1__item__para">
+                        <p className="work1__item__text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
+                      </div>
+                    </div>
+                    <div className="work1__item">
+                      <div className="work1__item__title">
+                        <span><p><i class="fa fa-video-camera"></i> Real Project For Real Solutions</p></span>
+                      </div>
+                      <div className="work1__item__para">
+                        <p className="work1__item__text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
+                      </div>
+                    </div>
                   </div>
-                  <div className="item_work1__para">
-                    <p className="font-4">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
-                  </div>
-                </div>
-                <div className="item_work1">
-                  <div className="item_work1__title">
-                    <span><p><i class="fa fa-video-camera"></i> Real Project For Real Solutions</p></span>
-                  </div>
-                  <div className="item_work1__para">
-                    <p className="font-4">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
-                  </div>
-                </div>
-                <div className="item_work1">
-                  <div className="item_work1__title">
-                    <span><p><i class="fa fa-video-camera"></i> Real Project For Real Solutions</p></span>
-                  </div>
-                  <div className="item_work1__para">
-                    <p className="font-4">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
+                  <div className="work1__item">
+                    <div className="work1__item__title">
+                      <span><p><i class="fa fa-video-camera"></i> Real Project For Real Solutions</p></span>
+                    </div>
+                    <div className="work1__item__para">
+                      <p className="work1__item__text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="app__main__plans">
-            <div className="app__main__plans__content">
-              <h1 className="font-5">Plans Built For Every One</h1>
-              <p className="font-4">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum socilis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. </p>
+          <div className="pricing">
+            <div className="pricing__content">
+              <h1 className="pricing__title">Plans Built For Every One</h1>
+              <p className="pricing__text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum socilis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. </p>
               < hr className="gachphancach" />
             </div>
-            <div className="app__main__plans__price">
-              <div className="item_price">
-                <p className="font-6">STARTER</p>
-                <div className="item_price__charge">
+            <div className="pricing__detail">
+              <div className="detail">
+                <p className="detail__title">STARTER</p>
+                <div className="detail__charge">
                   <div className="charge1">$</div>
                   <div className="charge2">9</div>
                   <div className="charge3">/MONTH</div>
                 </div>
-                <p className="font-4">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum socilis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. </p>
+                <p className="pricing__text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum socilis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. </p>
                 <button className="btnselect">SELECT PLAN</button>
               </div>
-              <div className="item_price">
-                <p className="font-6">BASIC</p>
-                <div className="item_price__charge">
+              <div className="detail">
+                <p className="detail__title">BASIC</p>
+                <div className="detail__charge">
                   <div className="charge1">$</div>
                   <div className="charge2">27</div>
                   <div className="charge3">/MONTH</div>
                 </div>
-                <p className="font-4">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum socilis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. </p>
+                <p className="pricing__text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum socilis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. </p>
                 <button className="btnselect">SELECT PLAN</button>
               </div>
-              <div className="item_price">
-                <p className="font-6 special">PRO</p>
-                <p className="font-7">BEST OFFER</p>
-                <div className="item_price__charge">
+              <div className="detail">
+                <p className="detail__title special">PRO</p>
+                <p className="detail__offer">BEST OFFER</p>
+                <div className="detail__charge">
                   <div className="charge1">$</div>
                   <div className="charge2">74</div>
                   <div className="charge3">/MONTH</div>
                 </div>
-                <p className="font-4">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum socilis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. </p>
+                <p className="pricing__text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum socilis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. </p>
                 <button className="btnselect spe">SELECT PLAN</button>
               </div>
-              <div className="item_price">
-                <p className="font-6">UNLIMITED</p>
-                <div className="item_price__charge">
+              <div className="detail">
+                <p className="detail__title">UNLIMITED</p>
+                <div className="detail__charge">
                   <div className="charge1">$</div>
                   <div className="charge2">140</div>
                   <div className="charge3">/MONTH</div>
                 </div>
-                <p className="font-4">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum socilis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. </p>
+                <p className="pricing__text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum socilis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. </p>
                 <button className="btnselect">SELECT PLAN</button>
               </div>
             </div>
-            <div className="app__main__plans__content1">
-              <div className="font-4">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum socilis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.<a className="font-8" href="#">Learn More</a> </div>
+            <div className="pricing__content1">
+              <div className="pricing__text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum socilis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.<a className="font-8" href="#">Learn More</a> </div>
             </div>
           </div>
           <div className="app__main__services">
@@ -251,20 +263,20 @@ class App extends Component {
             <hr className="gachphancachfoot" />
             <div className="app__footer__item__address">
               <div className="addresscustom">
-                  <i className="fa fa-map-marker icon"></i>
-                  <p className="font-4"> Lorem ipsum dolor sit amet.</p>
+                <i className="fa fa-map-marker icon"></i>
+                <p className="font-4"> Lorem ipsum dolor sit amet.</p>
               </div>
-              
+
               <div className="addresscustom">
-                <i class="fa fa-phone icon"></i> 
+                <i class="fa fa-phone icon"></i>
                 <p className="font-4">+1235 2355 98</p>
               </div>
               <div className="addresscustom">
-                <i class="fa fa-envelope-o icon"></i> 
+                <i class="fa fa-envelope-o icon"></i>
                 <p className="font-4">info@yoursite.com</p>
               </div>
               <div className="addresscustom">
-                <i class="fa fa-globe icon"></i> 
+                <i class="fa fa-globe icon"></i>
                 <p className="font-4">www.yoursite.com</p>
               </div>
             </div>
